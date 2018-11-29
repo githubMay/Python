0749660a8cd76e2ce31cfb4a14c9851546b671d7
githubMay <38322350+githubMay@users.noreply.github.com>
2018-11-29 17:14:59 +0800

    Update main.py

--- hw04/main.py
+++ hw04/main.py
@@ -1,41 +1,21 @@
 import tornado.ioloop
 import tornado.web
-
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        html=''''''
-        for i in range(1, 10):
-            for j in range(1, i+1):
-                m='{}x{}={}\t'.format(j, i, i*j)
-                html+=m
-                #print(m, end='')
-            #print()
-            html+="<br>"
-        #print(html)
-
-        self.write(html)
-
 class ABCHandler(tornado.web.RequestHandler):
-    def get(self,m):
+    def get(self,x):
+        m=int(x) if x is not None else 9
         html=''''''
-        if int(m)<10:
-            for i in range(1, int(m)+1):
+        if m<10:
+            for i in range(1, m+1):
                 for j in range(1, i+1):
-                    #print('{}x{}={}\t'.format(j, i, i*j), end='')
-                    m='{}x{}={}\t'.format(j, i, i*j)
-                    html+=m
-                #print()
+                    a='{}x{}={}\t'.format(j, i, i*j)
+                    html+=a
                 html+="<br>"
             self.write(html)
         else:
             self.write("请输入一个不超过9的正整数")
-
-
 application = tornado.web.Application([
-    (r"/", MainHandler),
-    (r"/([0-9]+)", ABCHandler),
+    (r"/([0-9]+)?", ABCHandler),
 ],debug=True)
-
 if __name__ == "__main__":
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
